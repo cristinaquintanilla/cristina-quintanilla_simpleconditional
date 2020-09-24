7d5c5499254eaa73416db309eb186004a36e9900
cristinaquintanilla <70345076+cristinaquintanilla@users.noreply.github.com>
2020-09-24 14:52:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let celsius = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(celsius)
+    if (celsius == 10) {
+        basic.showString("Ski Time!")
+    } else if (celsius == 20) {
+        basic.showString("Picnic Time!")
+    } else if (celsius == 30) {
+        basic.showString("Beach Time!")
+    } else if (celsius == 40) {
+        basic.showString("It is better to stay at home")
+    } else {
+        basic.showNumber(celsius)
+    }
+})
+basic.forever(function () {
+    celsius = input.temperature()
+})
